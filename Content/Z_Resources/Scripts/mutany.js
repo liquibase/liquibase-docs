6396ed9bb1e5247a7d50fa0eb8991da3f58e30c0
cmccormick <cmccormick@datical.com>
2021-02-23 12:15:21 -0600

    PD-1350 Removing script as requested

--- Content/Z_Resources/Scripts/mutany.js
+++
@@ -1,3 +0,0 @@
-<script>(function(){var a=window.mutiny=window.mutiny||{};if(!window.mutiny.client){a.client={_queue:{}};var b=["identify","trackConversion"];var c=[].concat(b,["defaultOptOut","optOut","optIn"]);var d=function factory(c){return function(){for(var d=arguments.length,e=new Array(d),f=0;f<d;f++){e[f]=arguments[f]}a.client._queue[c]=a.client._queue[c]||[];if(b.includes(c)){return new Promise(function(b,d){a.client._queue[c].push({args:e,resolve:b,reject:d});setTimeout(d,500)})}else{a.client._queue[c].push({args:e})}}};c.forEach(function(b){a.client[b]=d(b)})}})();</script>
-
-<script src="https://client-registry.mutinycdn.com/personalize/client/9a74d33852d0d1db.js"></script>
