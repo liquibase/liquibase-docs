16105ac011da2cb77a83281061f2c7f15c15d4f6
ap-liquibase <apierre@liquibase.com>
2023-01-12 13:04:47 -0500

    DAT-12359 use data sources to get origin buckets from infra workspaces

--- scripts/redirect_creation/main.tf
+++ scripts/redirect_creation/main.tf
@@ -8,6 +8,30 @@ terraform {
   }
 }
 
+# use Terraform Remote State Data stanza to read state from prod and staging workspaces
+
+data "terraform_remote_state" "prod" {
+  backend = "remote"
+
+  config = {
+    organization = "liquibase"
+    workspaces = {
+      name = "liquibase-prod"
+    }
+  }
+}
+
+data "terraform_remote_state" "staging" {
+  backend = "remote"
+
+  config = {
+    organization = "liquibase"
+    workspaces = {
+      name = "liquibase-staging"
+    }
+  }
+}
+
 # Need legacy Datical account since s3 buckets for docs are there
 provider "aws" {
   region     = "us-east-1"
@@ -19,7 +43,7 @@ provider "aws" {
 # 0 byte objects used for redirects
 resource "aws_s3_object" "redirects" {
   count            = length(keys(var.redirects))
-  bucket           = var.stage == "prod" ? "prod-docsliquibasecom-origin" : "stage-docsstageliquibasecom-origin"
+  bucket           = var.stage == "prod" ? data.terraform_remote_state.prod.outputs.docs_origin_bucket : data.terraform_remote_state.staging.outputs.docs_origin_bucket
   key              = element(keys(var.redirects), count.index)
   content_type     = "text/html"
   website_redirect = element(values(var.redirects), count.index)
