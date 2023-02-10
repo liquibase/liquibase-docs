2e8153927f8415e719a8f96891fa185afab1dfdc
Jake Newton <jnewton@datical.com>
2023-02-10 16:11:55 -0600

    DAT-13438 fix redirects using terraform

--- scripts/redirect_creation/main.tf
+++ scripts/redirect_creation/main.tf
@@ -8,26 +8,17 @@ terraform {
   }
 }
 
-# use Terraform Remote State Data stanza to read state from staging workspace
-data "terraform_remote_state" "staging" {
+# use Terraform Remote State Data stanza to read state from environment-based workspace
+data "terraform_remote_state" "state" {
   backend = "remote"
   config = {
     organization = "liquibase"
     workspaces = {
-      name = "liquibase-staging"
+      name = "liquibase-${var.env}"
     }
   }
 }
-# use Terraform Remote State Data stanza to read state from prod workspace
-data "terraform_remote_state" "prod" {
-  backend = "remote"
-  config = {
-    organization = "liquibase"
-    workspaces = {
-      name = "liquibase-prod"
-    }
-  }
-}
+
 # Need legacy Datical account since s3 buckets for docs are there
 provider "aws" {
   alias      = "datical"
@@ -36,7 +27,7 @@ provider "aws" {
 # 0 byte objects used for redirects
 resource "aws_s3_object" "redirects" {
   count            = length(keys(var.redirects))
-  bucket           = data.terraform_remote_state."${var.env}".outputs.docs_origin_bucket
+  bucket           = data.terraform_remote_state.state.outputs.docs_origin_bucket
   key              = element(keys(var.redirects), count.index)
   content_type     = "text/html"
   website_redirect = element(values(var.redirects), count.index)
