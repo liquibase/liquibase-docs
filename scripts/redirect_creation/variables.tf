24ec31d98ebcbbee6015ef917cb7aa253fe9a51d
Sayali Mohadikar <76010603+sayaliM0412@users.noreply.github.com>
2023-06-21 10:43:04 -0700

    removing on pull/push to master from Publish Redirects to Staging for EnterpriseDocs (#190)
    Co-authored-by: Sayali M <sayali@Sayalis-MacBook-Pro>

--- scripts/redirect_creation/variables.tf
+++ scripts/redirect_creation/variables.tf
@@ -1,6 +1,7 @@
 # REDIRECT STORAGE FOR docs.liquibase.com: specifies redirect sources/destinations for all pagemoves and renames
 # See https://datical.atlassian.net/wiki/spaces/DOC/pages/2929426585/Working+in+the+MadCap+Flare+-+AWS+VM
 # DAT-12359 move map of redirects to https://github.com/Datical/liquibase-docs/tree/master/scripts
+
 variable "env" {
   type        = string
   description = "Environment to reference.  Should be staging || prod"
@@ -762,6 +763,6 @@ variable "redirects" {
 variable "enterprise_redirects" {
   type = map(string)
   default = {
-# Need to get a list of re-directs from @adrian or @amber
+  # Need to get a list of re-directs from @adrian or @amber
   }
 }
