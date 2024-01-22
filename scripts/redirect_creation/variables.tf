b840c4508a2774cb44215ccd736ca5a92f46958c
adrian-velonis1 <85172912+adrian-velonis1@users.noreply.github.com>
2024-01-22 16:59:31 -0500

    Syntax

--- scripts/redirect_creation/variables.tf
+++ scripts/redirect_creation/variables.tf
@@ -953,7 +953,7 @@ variable "redirects" {
     { key = "/workflows/liquibase-community/using-the-liquibase-percona-toolkit-extension.html", website_redirect = "https://contribute.liquibase.com/extensions-integrations/directory/database-tutorials/percona/percona-toolkit/" },
     { key = "/workflows/liquibase-community/using-the-jenkins-pipeline-stage-with-spinnaker.html", website_redirect = "https://contribute.liquibase.com/extensions-integrations/directory/integration-docs/Spinnaker/spinnaker-jenkins-pipeline-stage/" },
     { key = "/workflows/liquibase-community/using-the-run-job-pipeline-stage-with-spinnaker.html", website_redirect = "https://contribute.liquibase.com/extensions-integrations/directory/integration-docs/Spinnaker/spinnaker-runjob-pipeline-stage/" },
-    { key = "/xml/ns/dbchangelog/", website_redirect = "https://www.liquibase.org/xml/ns/dbchangelog/" },
+    { key = "/xml/ns/dbchangelog/", website_redirect = "https://www.liquibase.org/xml/ns/dbchangelog/" }
   ]
 }
 
